e16b53e89c086c8d3c5d9448199119b963621037
tarek <tarek90045@yahoo.com>
2019-10-27 16:11:04 +0200

    first

---
+++ dev/blockchain.js
@@ -0,0 +1,16 @@
+function Blockchain() {
+this.chain = [];
+this.newTransactions= []; 
+}
+
+
+Blockchain.prototype.createNewBlock= function (nonce,previousBlockHash,hash) {
+
+const newBlock= {
+    index: this.chain.length + 1,
+    timestamp: Date.now(),
+    transaction:this.newTransactions,
+    nonce: nonce,
+}
+    
+}
