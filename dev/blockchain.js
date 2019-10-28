23d23a277adbc81e71b101974a4594871139109d
tarek <tarek90045@yahoo.com>
2019-10-28 13:10:38 +0200

    second day

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,6 +1,6 @@
 function Blockchain() {
 this.chain = [];
-this.newTransactions= []; 
+this.pendingTransactions= []; 
 }
 
 
@@ -9,8 +9,31 @@ Blockchain.prototype.createNewBlock= function (nonce,previousBlockHash,hash) {
 const newBlock= {
     index: this.chain.length + 1,
     timestamp: Date.now(),
-    transaction:this.newTransactions,
+    transaction:this.pendingTransactions,
     nonce: nonce,
+    hash: hash,
+    previousBlockHash:previousBlockHash
+};
+    this.pendingTransactions=[];
+    this.chain.push(newBlock);
+    return newBlock;
 }
+
+Blockchain.prototype.getLastBlock = function () {
+    return this.chain[this.chain.length-1];
     
 }
+Blockchain.prototype.createNewTransaction = function (amount,sender,recipient) {
+ const newTransaction = {
+     amount:amount,
+     sender:sender,
+     recipient:recipient,
+ };
+
+    this.pendingTransactions.push(newTransaction);
+    
+    return this.getLastBlock()['index'] + 1;
+}
+
+
+module.exports = Blockchain;
---
+++ dev/test.js
@@ -0,0 +1,9 @@
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
+
+bitcoin.createNewBlock(13123123,'asda213153','123123edasd')
+
+bitcoin.createNewTransaction(100,'123912euduahbad','sadiahsdiqwheqwe21')
+
+console.log(bitcoin);
